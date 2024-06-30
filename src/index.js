d91ee50183c4ff62b50fbe87bd0bca1fb9fe8379
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2024-06-30 09:15:26 +0300

    save

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { setupServer } from './server.js';
-import { initMongoConnection } from './db/initMongoConnection.js';
-
-const bootstrap = async () => {
-  await initMongoConnection();
-  setupServer();
-};
-
-bootstrap();
